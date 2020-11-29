a4c9ec061e2dad4a6c4c3ebe8a8ab07dfc45e0ec
Nathan Iodice <nathan@iodice.co.uk>
2020-11-29 11:19:05 +0000

    Added Intro
    Added intro animation

---
+++ public/script.js
@@ -0,0 +1,28 @@
+var welcome = document.getElementById("welcome").classList;
+var logo = document.getElementsByClassName("biglogo")[0].classList;
+var myName = document.getElementById("myName").classList;
+var about = document.getElementById("about");
+function startUp() {
+    welcome.remove("hide");
+    welcome.add("appear");
+    setTimeout(function(){
+        welcome.add("hide");
+        logo.remove("hide");
+        logo.add("appear")
+        setTimeout(function(){
+            logo.add("hide");
+            myName.remove("hide");
+            myName.add("appear")
+            setTimeout(function(){
+                window.scroll({
+                    top: about.offsetTop, 
+                    left: 0, 
+                    behavior: 'smooth'
+                  });
+                  document.querySelector("html").classList.remove("noscroll");
+            },3200)
+        },3200)
+    },3200)
+}
+
+startUp()
